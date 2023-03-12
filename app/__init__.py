94e088cd971dbfca70c01650f5ae7257dcfafe75
Fajri Kornel <fajri.apk@gmail.com>
2023-03-12 20:38:01 +0700

    feat: change image hardcoded image list as txt file input

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import logging
 
+with open("image_list.txt", "r") as f:
+    image_urls = f.read().splitlines()
+
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 
--- app/views.py
+++ app/views.py
@@ -1,15 +1,7 @@
-from . import app, async_helper, util
+from . import app, async_helper, util, image_urls
 import time
 import requests
 import asyncio
-
-calls_to_make = [
-    "http://localhost:4567/125/https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
-    "http://localhost:4567/75/https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
-    "http://localhost:4567/100/https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
-    "http://localhost:4567/200/https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
-    "http://localhost:4567/500/https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80",
-]
 
 
 @app.route("/")
@@ -23,7 +15,7 @@ def sync_call():
     app.logger.info(f"STARTING AT: {start_time} s")
 
     image_list = []
-    for i, url in enumerate(calls_to_make):
+    for i, url in enumerate(image_urls):
         app.logger.info(f"EXECUTING NO. {i+1}: {url}")
         response = requests.get(url)
         image_list.append(response.content)
@@ -44,7 +36,7 @@ def async_call():
     start_time = time.perf_counter()
     app.logger.info(f"STARTING AT: {start_time} s")
 
-    asyncio.run(async_helper.execute_async_call(calls_to_make))
+    asyncio.run(async_helper.execute_async_call(image_urls))
 
     stop_time = time.perf_counter()
     app.logger.info(f"STOPPING AT: {stop_time} s")
